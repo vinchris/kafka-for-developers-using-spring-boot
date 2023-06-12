378afce0790da061bd7bba3f887a975ce66bb30d
Chris <ateamcristian@yahoo.de>
2023-06-12 14:29:48 +0300

    created-project
    just some basic configurations

---
+++ my-library-events-producer/src/main/java/com/learnkafka/MyLibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.learnkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryEventsProducerApplication.class, args);
+	}
+
+}
